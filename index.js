6055a84781fe01edaec68cb1873ec8637b6d3209
chadii-t <sadiaatraore30@gmail.com>
2023-07-27 18:28:35 +0000

    fixer la navbar

--- index.js
+++ index.js
@@ -6,4 +6,41 @@ toggle.addEventListener("click", function() {
 })
 //alert("ca marche");
 
-/**/
+/*<div class="container-10">
+      <div>
+      <p><img class="l5" src="omnifood-logo.png" alt=""></p>
+      <div class="social">
+      <img class="l1" src="logo-google.svg" alt="">
+      <img class="l1" src="logo-facebook.svg" alt="">
+      <img class="l1" src="logo-twitter.svg" alt="">
+      </div>
+      <p>Copyright © 2027 by Omnifood,</p><p> Inc. All rights reserved.</p>
+      </div>
+      
+      <div><h1>Contact us</h1>
+      <p>623 Harrison St., 2nd Floor,</p><p> San Francisco, CA 94107</p>
+      
+      <p>415-201-6370</p>
+      <p>hello@omnifood.com</p>
+      </div>
+      
+      <div><h1>Account</h1>
+        <p>  Create account</p>
+        <p>  Sign in</p>
+        <p> iOS app</p>
+         <p> Android app</p>
+      </div>
+      
+      <div><h1>Company</h1>
+       <p>   About Omnifood</p>
+         <p> For Business</p>
+        <p>  Cooking partners</p>
+         <p> Careers</p>
+      </div>
+      
+      <div><h1>Ressources</h1>
+        <p>  Recipe directory </p>
+         <p> Help center </p>
+         <p> Privacy & terms </p>
+      </div>
+      */
